65091a4bd3e1b56a617d14d4fb2a8eee9e19fbf1
samakshkunden <samakshkunden@gmail.com>
2026-03-05 20:40:29 +0530

    Add LeetCode Palindrome Number solution (digit reverse)

---
+++ lc_9_palindrome_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        int original = x;
+        double rev = 0;
+
+        while (x > 0)
+        {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == rev;
+    }
+};
